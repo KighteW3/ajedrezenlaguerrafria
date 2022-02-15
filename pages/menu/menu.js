4799e1fcdf25d5ec16304c8de3b75394e76f4057
KighteW3 <99749798+KighteW3@users.noreply.github.com>
2022-02-15 23:24:16 +0100

    Add files via upload

---
+++ pages/menu/menu.js
@@ -0,0 +1,23 @@
+function link1() {
+    window.location = "../../index.html";
+}
+
+function link2() {
+    window.location = "../page1/resumen.html";
+}
+
+function link3() {
+    window.location = "../page2/spassky.html";
+}
+
+function link4() {
+    window.location = "../page3/fischer.html";
+}
+
+function link5() {
+    window.location = "../page4/transcursoJuego.html";
+}
+
+function link6() {
+    window.location = "../finalpage/final.html";
+}
