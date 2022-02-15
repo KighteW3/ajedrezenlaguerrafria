ce2df3e693e25f1ec1ca9b2317070d1d18b392d2
KighteW3 <99749798+KighteW3@users.noreply.github.com>
2022-02-15 23:22:10 +0100

    Add files via upload

---
+++ pages/page1/resumen.js
@@ -0,0 +1,55 @@
+function previousPage() {
+    window.location = "../../index.html"
+}
+
+function nextPage2() {
+    window.location = "../page2/spassky.html";
+}
+
+function imgClick1() {
+    window.open("1.jpg");
+}
+
+function imgClick2() {
+    window.open("spasskyvsfischer.jpg");
+}
+
+function imgClick3() {
+    window.open("3.jpg");
+}
+
+function imgClick4() {
+    window.open("4.jpg");
+}
+
+function imgClick5() {
+    window.open("5.jpg");
+}
+
+function imgClick6() {
+    window.open("6.jpg");
+}
+
+function imgClick7() {
+    window.open("7.jpg");
+}
+
+function imgClick8() {
+    window.open("8.jpg");
+}
+
+function imgClick9() {
+    window.open("9.jpg");
+}
+
+function imgClick10() {
+    window.open("10.jpg");
+}
+
+function imgClick11() {
+    window.open("11.jpg");
+}
+
+function imgClick12() {
+    window.open("12.jpg");
+}
